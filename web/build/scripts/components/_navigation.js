6daf5e832ea45a1e2ab0eac4a026232f727fc482
ayana powell <ayanapowell1@gmail.com>
2016-12-21 16:03:30 -0800

    media queries to homepage

--- web/build/scripts/components/_navigation.js
+++ web/build/scripts/components/_navigation.js
@@ -2,33 +2,6 @@ var app = app || {};
 (function($){
   app.navigation = {
 // when menu icon clicked, show navigation menu, darken the rest of content
-    showNav : function () {
-      app.navigation.els.test.click(function() {
-        $(this).toggleClass('active');
-        app.navigation.els.navBlock.css({ // display navigation
-          'display' : 'block',
-          "opacity" : 0.99,
-          "transition" : "0s"
-        });
-        app.navigation.els.heroTagline.css({ // hide tagline
-          'opacity' : '0',
-          'transition' : '0.5s ease'
-        });
-        app.navigation.els.overlay.animate({ // darken window to show nav items
-          "opacity" : 0.99,
-          "transition" : "0s"
-        });
-        setTimeout(function() {
-          app.navigation.els.navItems.each(function(i, el) { // fade in nav-items
-            var el = $(el);
-            el.css({
-              'opacity': '1',
-              'transition' : '0.8s ease all'
-            });
-          });
-        }, 200);
-      });
-    },
     toggleNav : function () {
       app.navigation.els.test.click(function() {
         $(this).toggleClass('active'); // from menu to x button
@@ -36,7 +9,7 @@ var app = app || {};
         app.navigation.els.heroTagline.toggleClass('hide-block'); // hide tagline
         app.navigation.els.overlay.toggleClass('show-block');// darken window to show nav items
         $('img.hero__logo').css({
-          'z-index': 100
+          'z-index': 10
         })
         setTimeout(function() {
           app.navigation.els.navItems.each(function(i, el) { // fade in nav-items
