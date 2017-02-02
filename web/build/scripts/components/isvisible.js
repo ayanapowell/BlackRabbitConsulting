6050f53c63bdb8d53479aabb64f19633aa9bf6dc
ayana powell <ayanapowell1@gmail.com>
2017-02-01 23:37:16 -0800

    start the services page

--- web/build/scripts/components/isvisible.js
+++ web/build/scripts/components/isvisible.js
@@ -4,11 +4,13 @@ var app = app || {};
    fadeIntoView : function () {
       setTimeout(function() {
         app.isVisible.els.tagline.addClass('show');
+        app.isVisible.els.tagline2.addClass('show');
       }, 500);
    },
     init : function() {
       app.isVisible.els = {
     	  tagline : $('.container__hero-tagline'),
+        tagline2 : $('.hero__punchline'),
       };
       app.isVisible.fadeIntoView();
     }
